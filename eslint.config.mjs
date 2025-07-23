322cf9ea50feb669f41ab1e282cac759674cdc65
yhdeon <deonprojdev@hotmail.com>
2025-07-23 16:43:40 +0800

    add codeql

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,33 @@
-import js from "@eslint/js";
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+//   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+// ]);
+
+
 import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginSecurity from "eslint-plugin-security";
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      security: pluginSecurity
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "security/detect-eval-with-expression": "error",
+    }
+  }
+];
---
+++ test.js
@@ -0,0 +1,3 @@
+// test.js
+const expression = '1 + 1';
+eval(`console.log(${expression})`); // This evaluates the expression 1 + 1
